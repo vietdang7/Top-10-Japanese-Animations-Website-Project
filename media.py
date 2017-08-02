fed6c20a5fef67c6774cf26e232700c6dc915710
Viet Dang Quoc <thedangs7@gmail.com>
2017-08-02 20:42:00 +0300

    Create class Animation in media.py

---
+++ media.py
@@ -0,0 +1,16 @@
+# Import webbrowser to use open()
+import webbbrowser
+
+# Create class Animation - follow Google guide
+class Animation():
+    """This class provides structure to store Japanese animations information """
+
+    # Start __init__
+    def __init__(self, movie_title, movie_poster, youtube_url):
+        self.title = movie_title
+        self.poster = movie_poster
+        self.trailer = youtube_url
+
+    # Play trailer function
+    def play_trailer(self):
+        webbrowser.open(self.trailer)
